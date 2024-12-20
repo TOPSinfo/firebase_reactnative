9aeedce2af421b902902b8a43e6dcab11a7d214c
Nishit <nishit@Nishits-Mac-Pro.local>
2024-12-20 18:48:17 +0530

    working on jitsi meet component

--- app/(home)/videocall.tsx
+++ app/(home)/videocall.tsx
@@ -23,16 +23,31 @@ const videocall = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <JitsiMeeting
-        config={{}}
-        eventListeners={eventListeners as any}
-        ref={jitsiMeeting}
-        style={{ flex: 1 }}
-        room={'astro_appointment'}
-        serverURL={'https://meet.jit.si/'}
-      />
-    </View>
+    <JitsiMeeting
+      config={{
+        hideConferenceTimer: true,
+      }}
+      eventListeners={eventListeners as any}
+      ref={jitsiMeeting}
+      style={{ flex: 1 }}
+      flags={{
+        'audioMute.enabled': true,
+        'ios.screensharing.enabled': true,
+        'fullscreen.enabled': false,
+        'audioOnly.enabled': false,
+        'android.screensharing.enabled': true,
+        'pip.enabled': false,
+        'pip-while-screen-sharing.enabled': false,
+        'conference-timer.enabled': true,
+        'close-captions.enabled': false,
+        'toolbox.enabled': true,
+        'meeting-name.enabled': false,
+      }}
+      room={'astro_appointment'}
+      // serverURL={'https://meet.jit.si/'}
+      serverURL="https://meet.ffmuc.net/"
+      //serverURL="https://meet.guifi.net"
+    />
   );
 };
 
